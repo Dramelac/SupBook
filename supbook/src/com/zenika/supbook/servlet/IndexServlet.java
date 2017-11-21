a18f06ead9424bf8bdd38960fbff9916469c129c
Dramelac <203697@supinfo.com>
2017-11-21 17:39:06 +0100

    Fix artifact web classes

--- supbook/src/com/zenika/supbook/servlet/IndexServlet.java
+++ supbook/src/com/zenika/supbook/servlet/IndexServlet.java
@@ -2,15 +2,16 @@ package com.zenika.supbook.servlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "index", urlPatterns = "/index")
-public class IndexServlet {
+public class IndexServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/index.jsp").forward(request, response);
+        request.getRequestDispatcher("/public/index.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
