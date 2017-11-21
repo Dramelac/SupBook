92ce03d91cbca27f351c75e42e2e9f2851902fc6
Droopy <romain.garcia38230@hotmail.com>
2017-11-21 14:41:51 +0100

    création du package servlet

---
+++ supbook/src/com/zenika/supbook/servlet/IndexServlet.java
@@ -0,0 +1,14 @@
+package com.zenika.supbook.servlet;
+
+
+public class IndexServlet {
+
+    /*protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }*/
+
+}
