e45717d34803e80b4438663cd1f8df9f0b5eabf0
Droopy <romain.garcia38230@hotmail.com>
2017-11-21 15:04:54 +0100

    modif artifact

--- supbook/src/com/zenika/supbook/servlet/IndexServlet.java
+++ supbook/src/com/zenika/supbook/servlet/IndexServlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class IndexServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
