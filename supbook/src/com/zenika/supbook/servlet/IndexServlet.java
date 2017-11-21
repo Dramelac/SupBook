f19f920fb9070270287759c6bc5cd0586ec99f19
Droopy <romain.garcia38230@hotmail.com>
2017-11-21 14:53:34 +0100

    lib pour les servlet

--- supbook/src/com/zenika/supbook/servlet/IndexServlet.java
+++ supbook/src/com/zenika/supbook/servlet/IndexServlet.java
@@ -1,14 +1,20 @@
 package com.zenika.supbook.servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet(name = "index", urlPatterns = "/index")
 public class IndexServlet {
 
-    /*protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }*/
+    }
 
 }
