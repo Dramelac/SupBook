2a7ba20c73f46eed57e714af7ba21435c88d40d9
Dorian <208108@supinfo.com>
2017-11-22 14:31:29 +0100

    Modification FriendRequest

--- supbook/src/com/zenika/supbook/model/FriendRequest.java
+++ supbook/src/com/zenika/supbook/model/FriendRequest.java
@@ -7,17 +7,20 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.Date;
 
+@Entity
 public class FriendRequest implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
-    @JoinColumn(name = "OwnerID")
-    private int OwnerID;
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="OwnerID")
+    private User owner;
 
-    @JoinColumn(name = "ReceiverID")
-    private int ReceiverID;
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="ReceiverID")
+    private User receiver;
 
     @Column(unique = false )
     private Date createAt;
@@ -25,5 +28,28 @@ public class FriendRequest implements Serializable {
     @Column(unique = false)
     private Boolean status = false;
 
+    public FriendRequest(){
+
+    }
+
+    public int getId() {return id;}
+
+    public void setId(int id) {this.id = id;}
+
+    public User getOwner() {return owner;}
+
+    public void setOwner(User owner) {this.owner = owner;}
+
+    public User getReceiver() {return receiver;}
+
+    public void setReceiver(User receiver) {this.receiver = receiver;}
+
+    public Date getCreateAt() {return createAt;}
+
+    public void setCreateAt(Date createAt) {this.createAt = createAt;}
+
+    public Boolean getStatus() {return status;}
+
+    public void setStatus(Boolean status) {this.status = status;}
 }
 
