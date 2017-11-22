1caf91a8b956059c212ec16e1338bc6f013e7567
Dramelac <203697@supinfo.com>
2017-11-22 09:41:06 +0100

    First DAL implementation

--- supbook/src/com/zenika/supbook/model/User.java
+++ supbook/src/com/zenika/supbook/model/User.java
@@ -1,18 +1,41 @@
 package com.zenika.supbook.model;
 
-public class User extends AbstractBusinessObject {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@Column(unique=true)
 	private String login;
+
+	@Column(unique=false)
+	private String password;
+
+	@Column(unique=true)
 	private String email;
+
+	@Column(unique=false)
+	private boolean isAdmin;
 
 	public User() {
 	}
-	
-	public User(long id, String login, String email) {
-		this.setId(id);
+
+	public User(String login, String password, String firstname, String lastname, String email, String address, String phonenumber, boolean isAdmin) {
 		this.login = login;
+		this.password = password;
 		this.email = email;
+		this.isAdmin = false;
 	}
-	
+
+	public int getId() {
+		return id;
+	}
+
 	public String getLogin() {
 		return login;
 	}
@@ -21,12 +44,26 @@ public class User extends AbstractBusinessObject {
 		this.login = login;
 	}
 
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public String getEmail() {
 		return email;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public boolean isAdmin() {
+		return isAdmin;
 	}
-	
+
+	public boolean getIsAdmin() {return isAdmin;}
+
+	public void setAdmin(boolean admin) {
+		isAdmin = admin;
+	}
+
 }
