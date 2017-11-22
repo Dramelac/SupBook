bb3eb9c3500c19fcfd35df38e5b5bf4c0f201c16
Grand maitre <204343@supinfo.com>
2017-11-22 17:24:05 +0100

    ajout page friend

---
+++ SupBookv2/src/com/supinfo/supbook/servlet/user/FriendProfileServlet.java
@@ -0,0 +1,13 @@
+package com.supinfo.supbook.servlet.user;
+
+
+import com.supinfo.supbook.DAL.UserDAO;
+import com.supinfo.supbook.entity.User;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "FriendProfileServlet", urlPatterns = "/user/profile")
+public class FriendProfileServlet {
+    User user = UserDAO.getUserById((int) request.getSession().getAttribute("userId"));
+    user.setEmail(email);
+}
