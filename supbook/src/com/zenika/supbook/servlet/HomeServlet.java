20f5144d230d63fa3add42b79933eacf00a7bb81
Antoine <209745@supinfo.com>
2017-11-22 11:00:05 +0100

    getUserFromSearch function

--- supbook/src/com/zenika/supbook/servlet/HomeServlet.java
+++ supbook/src/com/zenika/supbook/servlet/HomeServlet.java
@@ -1,11 +1,14 @@
 package com.zenika.supbook.servlet;
 
+import com.zenika.supbook.model.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/HomeServlet")
@@ -25,7 +28,10 @@ public class HomeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
-    private void getUsersFromSearch()
+    private List<User> getUsersFromSearch()
     {
+        ArrayList<User> userList = new ArrayList<>();
+        return userList;
+
     }
 }
